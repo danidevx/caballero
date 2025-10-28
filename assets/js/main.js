104f45fc94d37ca4f34a21d45d1373ef52ff3c4d
danidevx <deanarespe@gmail.com>
2025-10-28 05:07:32 +0100

    Caballeros

---
+++ assets/js/main.js
@@ -0,0 +1,101 @@
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
+
+/* Menu Show */
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
+
+/* Menu Hidden */
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
+
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll(".nav__link");
+
+const linkAction = () => {
+  const navMenu = document.getElementById("nav-menu");
+  // When we click on each nav__link, we remove the show-menu class
+  navMenu.classList.remove("show-menu");
+};
+
+navLink.forEach((n) => n.addEventListener("click", linkAction));
+
+/*=============== SWIPER FRANELAS ===============*/
+const swiperFranelas = new Swiper(".franelas__swiper", {
+  loop: true,
+  grabCursor: true,
+  slidesPerView: "auto",
+
+  navigation: {
+    nextEl: "#franelas-next",
+    prevEl: "#franelas-prev",
+  },
+
+  breakpoints: {
+    768: {
+      slidesPerView: 3,
+      centeredSlides: "auto",
+    },
+    1152: {
+      centeredSlides: "auto",
+      spaceBetween: -64,
+    },
+  },
+});
+
+/*=============== SWIPER PANTS ===============*/
+const swiperPants = new Swiper(".home__swiper", {
+  loop: true,
+  grabCursor: true,
+  slidesPerView: "auto",
+
+  navigation: {
+    nextEl: "#pants-next",
+    prevEl: "#pants-prev",
+  },
+
+  breakpoints: {
+    768: {
+      slidesPerView: 3,
+      centeredSlides: "auto",
+    },
+    1152: {
+      centeredSlides: "auto",
+      spaceBetween: -64,
+    },
+  },
+});
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+const bgHeader = () => {
+  const header = document.getElementById("header");
+  // Add a class if the bottom offset is greater than 50 of the viewport
+  this.scrollY >= 50
+    ? header.classList.add("bg-header")
+    : header.classList.remove("bg-header");
+};
+window.addEventListener("scroll", bgHeader);
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "60px",
+  duration: 2500,
+  // reset: true, // Animation Repeat
+});
+
+sr.reveal(`.franelas__swiper, .home__swiper, .home__footer`);
+sr.reveal(`.home__circle`, { scale: 1.5, delay: 100 });
+sr.reveal(`.home__subcircle`, { rotate: { z: 360 }, duration: 1000, delay: 300, origin: 'left' });
+sr.reveal(`.home__title`, { scale: 1, origin: "bottom", delay: 900 });
+sr.reveal(`.swiper-button-prev, .swiper-button-next`, {
+  origin: "bottom",
+});
